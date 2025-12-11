432d05f51ed897559e621ca2eab0ee62142649cd
Partan421 <myzahid@hiof.no>
2025-12-11 01:52:22 +0100

    Update oppgave2_1.py

--- oppgave2/deloppgave1/oppgave2_1.py
+++ oppgave2/deloppgave1/oppgave2_1.py
@@ -15,12 +15,19 @@ prw = [
 ]
 
 #2
+#lager en funksjon kalt for lungo, den tar imot info fra parameteren ordliste
+#if delen sjekker først om listen er tom, men fordi den bruker not blir resultatet true hvis listen er tom, hvis ikke returneren den funksjonen None
+#Return max leter etter det lengste ordet i listen.
+#key=len leter etter det lengste ordet i ordlisten i dette tilfellet.
 def lungo(ordliste):
     if not ordliste:
         return None
     return max(ordliste, key=len)
 
 #3
+#lager en ny funksjon kalt for add og tar imot en variabel kalt for nytt:ord
+#prw.append legger order inn i listen/array prw.
+#strip fjerner mellomrom før og etter ordet.
 def add(nytt_ord):
     prw.append(nytt_ord.strip())
 
@@ -29,5 +36,7 @@ def add(nytt_ord):
 add("objektorientert")
 
 #5
+#resultat lagrer verdien fra lungo(prw)
+#Print skriver ut Lengst: + verdien lagret i resultat
 resultat = lungo(prw)
 print("Lengst:", resultat)
